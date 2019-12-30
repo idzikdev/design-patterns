858ea338200ef6d422ed14a1174986a820f705b0
Tomasz Idzikowski <idzikqa@gmail.com>
2019-12-30 15:20:09 +0100

    Interpreter src init - two samples

---
+++ Interpreter/src/main/java/numbers/Number.java
@@ -0,0 +1,15 @@
+package numbers;
+
+public class Number implements Expression {
+
+    private final int n;
+
+    public Number(int n) {
+        this.n = n;
+    }
+
+    @Override
+    public int interpret() {
+        return n;
+    }
+}
---
+++ Interpreter/src/main/java/numbers/Utils.java
@@ -0,0 +1,23 @@
+package numbers;
+
+public class Utils {
+
+    public static boolean isOperator(String s) {
+        if (s.equals("+") || s.equals("-") || s.equals("*"))
+            return true;
+        else
+            return false;
+    }
+
+    public static Expression getOperator(String s, Expression left, Expression right) {
+        switch (s) {
+            case "+":
+                return new Add(left, right);
+            case "-":
+                return new Substract(left, right);
+            case "*":
+                return new Product(left, right);
+        }
+        return null;
+    }
+}
