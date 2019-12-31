01dd329f4e10007fd180694cae0f36cea6c3e91a
Tomasz Idzikowski <idzikqa@gmail.com>
2019-12-31 10:22:17 +0100

    Iterator src init

---
+++ Iterator/src/main/java/iterator/Iterator.java
@@ -0,0 +1,11 @@
+package iterator;
+
+public interface Iterator<T> {
+    void reset();   // reset to the first element
+
+    T next();   // To get the next element
+
+    T currentItem();    // To retrieve the current element
+
+    boolean hasNext();  // To check whether there is any next element or not.
+}
---
+++ Iterator/src/main/java/iterator/List.java
@@ -0,0 +1,6 @@
+package iterator;
+
+public interface List<E>
+{
+    Iterator<E> iterator();
+}
